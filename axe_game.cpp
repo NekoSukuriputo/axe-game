c307e1898f6959ed1b8888070a097c6280e66a5d
abdul <abdul.rahmat@360kredi.id>
2024-03-07 15:10:02 +0700

    23. Collision Detection (add circle and axe edges)

--- axe_game.cpp
+++ axe_game.cpp
@@ -11,10 +11,22 @@ int main()
     int circle_x{200};
     int circle_y{200};
     int circle_radius{25};
+    // circle_edges
+    int l_circle_x{circle_x - circle_radius};
+    int r_circle_x{circle_x + circle_radius};
+    int u_circle_y{circle_y - circle_radius};
+    int b_circle_y{circle_y + circle_radius};
 
     // axe coordinates
     int axe_x{400};
     int axe_y{0};
+    int axe_length{50};
+    // axe edges
+    int l_axe_x{axe_x};
+    int r_axe_x{axe_x + axe_length};
+    int u_axe_y{axe_y};
+    int b_axe_y{axe_y + axe_length};
+
 
     int direction{10};
     
@@ -27,11 +39,11 @@ int main()
         // Game logic begins
 
         DrawCircle(circle_x, circle_y, circle_radius, BLUE);
-        DrawRectangle(axe_x, axe_y, 50, 50, RED);
+        DrawRectangle(axe_x, axe_y, axe_length, axe_length, RED);
 
         // move the axe
         axe_y += direction;
-        if(axe_y > 450 || axe_y < 0)
+        if(axe_y > height || axe_y < 0)
         {
             direction = -direction;
         }
