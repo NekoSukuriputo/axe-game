811f0cd3930c1dbf31d973c6af432c8189041280
abdul <abdul.rahmat@360kredi.id>
2024-03-07 14:40:40 +0700

    19. Moving the circle

--- axe_game.cpp
+++ axe_game.cpp
@@ -2,18 +2,37 @@
 
 int main()
 {
-    int width = 350;
-    int height = 200;
+    // Window dimentions
+    int width{350};
+    int height{200};
     InitWindow(width, height, "Axe Game Learn C++");
 
+    // Circle coordinate
+    int circle_x{175};
+    int circle_y{100};
+    int circle_radius{25};
+
+
+    SetTargetFPS(60);
     while (WindowShouldClose() != true)
     {
         BeginDrawing();
 
         ClearBackground(WHITE);
+        // Game logic begins
 
-        DrawCircle(175, 100, 25, BLUE);
+        DrawCircle(circle_x, circle_y, circle_radius, BLUE);
 
+        if(IsKeyDown(KEY_D))
+        {
+            circle_x = circle_x + 10;
+        }
+        if(IsKeyDown(KEY_A))
+        {
+            circle_x = circle_x - 10;
+        }
+
+        // Game login ends
         EndDrawing();
     }
 }
