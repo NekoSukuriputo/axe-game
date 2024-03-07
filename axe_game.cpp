dca1fccda98ea5b2d5b5f15296241cb13e983d64
abdul <abdul.rahmat@360kredi.id>
2024-03-07 14:44:05 +0700

    20. Logical AND (Moving circle)

--- axe_game.cpp
+++ axe_game.cpp
@@ -23,11 +23,11 @@ int main()
 
         DrawCircle(circle_x, circle_y, circle_radius, BLUE);
 
-        if(IsKeyDown(KEY_D))
+        if(IsKeyDown(KEY_D) && circle_x < 350)
         {
             circle_x = circle_x + 10;
         }
-        if(IsKeyDown(KEY_A))
+        if(IsKeyDown(KEY_A) && circle_x > 0)
         {
             circle_x = circle_x - 10;
         }
